ca253396240893ebb61acd031cbde5b84dd6c836
Anil Kumar krishanshetty <anilbms75@gmail.com>
2019-01-06 09:54:44 +0100

    Working with CSS

--- examples/webpack-react-scss/src/button/button.tsx
+++ examples/webpack-react-scss/src/button/button.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {Helmet} from "react-helmet";
 import { Button as _Button } from "react-bootstrap";
 import './button.css';
 interface ButtonProps {
@@ -14,7 +15,19 @@ export class Button extends React.Component<ButtonProps> {
   };
 
   render() {
-    return (<_Button {...this.props}> { this.props.text } </_Button>);
+    return (<div>
+      <Helmet
+        link={[
+          {
+            href: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+            rel: "stylesheet",
+            type: "text/css",
+            media: "all"
+          }
+        ]}
+      />
+      <_Button {...this.props}> { this.props.text } </_Button>
+    </div>);
   }
 }
 export default Button;
